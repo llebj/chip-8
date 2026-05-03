7b3db8910a250ad65cb652823cccb48edc45431a
Joe Bell <35937399+llebj@users.noreply.github.com>
2026-05-03 10:11:41 +0000

    Introduced emulator loop.

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
+#define _POSIX_C_SOURCE 199309L
+
+#include <stdio.h>
+#include <stdlib.h>
+// `clock_gettime` requires _POSIX_C_SOURCE >= 199309L
+#include <time.h>
+
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <SDL3/SDL.h>
 
 #define MEM_SIZE 4096
@@ -11,7 +15,16 @@
 #define ROM_START 0x200	// 512
 #define ROM_MAX 3894
 
+#define NANOSEC 1000000000L
+
 unsigned char mem[MEM_SIZE];
+unsigned long pc = 0;
+unsigned int i_reg = 0;
+unsigned int stack[16];
+unsigned char d_timer = 0;
+unsigned char s_timer = 0;
+unsigned char v_reg[16];
+
 unsigned char const font[] = {
 	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
 	0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -43,22 +56,32 @@ int main(int argc, char** argv)
 		exit(0);
 	}
 
-	SDL_Window *window = NULL;
-	SDL_Renderer *renderer = NULL;
+	const double period = 1.0 / 700;	// The target seconds per instruction
+	double diff = 0,
+	       remaining = 0;
+	struct timespec t0,
+			t1,
+			wait;
 
-	SDL_SetAppMetadata("CHIP-8", "0.0.0", "com.llebj.chip-8");
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer("CHIP-8", 640, 320, SDL_WINDOW_MAXIMIZED, &window, &renderer);
-	SDL_SetRenderLogicalPresentation(renderer, 64, 32, SDL_LOGICAL_PRESENTATION_LETTERBOX);
-
-	SDL_SetRenderDrawColorFloat(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE_FLOAT);
-	/* clear the window to the draw color. */
-	SDL_RenderClear(renderer);
-	/* put the newly-cleared rendering on the screen. */
-	SDL_RenderPresent(renderer);
+	struct timespec delay;
+	delay.tv_sec = 0;
+	delay.tv_nsec = 500000L;		// 500 micro-seconds
 
 	while (1) {
-		continue;
+		clock_gettime(CLOCK_MONOTONIC, &t0);
+		// Simulate work
+		nanosleep(&delay, NULL);
+		clock_gettime(CLOCK_MONOTONIC, &t1);
+
+		// Throttle execution time to `period`
+		diff = (t1.tv_sec - t0.tv_sec) + 1.0e-9 * (t1.tv_nsec - t0.tv_nsec);
+		remaining = period - diff;
+		printf("time elapsed: %fs; waiting: %fs\n", diff, remaining > 0 ? remaining : 0);
+		if (remaining > 0) {
+			wait.tv_sec = (long)remaining;
+			wait.tv_nsec = (long)(remaining * NANOSEC) % NANOSEC;
+			nanosleep(&wait, NULL);
+		}
 	}
 
 	// Copy the font data into memory starting at `mem + FONT_START`.
@@ -67,10 +90,6 @@ int main(int argc, char** argv)
 	rom_size = load_rom(argv[1], mem + ROM_START);
 	dump_rom(mem + ROM_START, rom_size);
 	*/
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 }
 
 void dump_rom(unsigned char* rom, int len)
@@ -106,4 +125,32 @@ int load_rom(char* path, unsigned char* buffer)
 
 	fclose(fp);
 	return len;
+}
+
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+void sdl_init()
+{
+	SDL_SetAppMetadata("CHIP-8", "0.0.0", "com.llebj.chip-8");
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer("CHIP-8", 640, 320, SDL_WINDOW_MAXIMIZED, &window, &renderer);
+	SDL_SetRenderLogicalPresentation(renderer, 64, 32, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+}
+
+void sdl_destroy()
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void draw()
+{
+	SDL_SetRenderDrawColorFloat(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE_FLOAT);
+	/* clear the window to the draw color. */
+	SDL_RenderClear(renderer);
+	/* put the newly-cleared rendering on the screen. */
+	SDL_RenderPresent(renderer);
 }
