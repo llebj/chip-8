ee70b68c5ed33030b3cfff0e4f00c6a1d844ea39
Joe Bell <35937399+llebj@users.noreply.github.com>
2026-05-20 09:59:29 +0000

    Started using fixed width integer types.

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #define _POSIX_C_SOURCE 199309L
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 // `clock_gettime` requires _POSIX_C_SOURCE >= 199309L
 #include <time.h>
 
@@ -17,15 +19,15 @@
 
 #define NANOSEC 1000000000L
 
-unsigned char memory[MEM_SIZE];
-unsigned char *pc = NULL;
-unsigned int i = 0;
-unsigned int stack[16];
-unsigned char delay = 0;
-unsigned char sound = 0;
-unsigned char v[16];
+uint8_t memory[MEM_SIZE];
+uint8_t *pc = NULL;
+uint16_t i = 0;
+uint16_t stack[16];
+uint8_t delay = 0;
+uint8_t sound = 0;
+uint8_t v[16];
 
-unsigned char const font[] = {
+uint8_t const font[] = {
 	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
 	0x20, 0x60, 0x20, 0x20, 0x70, // 1
 	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -46,30 +48,30 @@ unsigned char const font[] = {
 
 int rom_size = 0;
 
-void dump_rom(unsigned char* rom, int len);
-int load_rom(char* path, unsigned char* buffer);
+void dump_rom(uint8_t* rom, int len);
+int load_rom(char* path, uint8_t* buffer);
 
 int main(int argc, char** argv)
 {
-	if (argc != 2) {
-		printf("Usage:\n\t%s { rom_file_path }\n", argv[0]);
+	if (argc < 2) {
+		printf("Usage:\n\t%s { rom_file_path }\n\t%s --dump-rom { rom_file_path }\n", argv[0], argv[0]);
 		exit(0);
 	}
 
 	// Copy the font data into memory starting at `memory + FONT_START`.
 	memcpy(memory + FONT_START, font, sizeof(font));
-	rom_size = load_rom(argv[1], memory + ROM_START);
-	/*
-	dump_rom(mem + ROM_START, rom_size);
-	*/
+	rom_size = load_rom(argv[argc - 1], memory + ROM_START);
+
+	if (strcmp(argv[1], "--dump-rom") == 0) {
+		dump_rom(memory + ROM_START, rom_size);
+		exit(0);
+	}
 
 	// Move the program counter to the start of the ROM
 	pc = memory + ROM_START;
 
-	// Storage variables used when custructing the current instruction during
-	// the fetch stage of the loop.
-	unsigned int current = 0,
-		     temp = 0;
+	// Storage for the current opcode read during the fetch loop.
+	uint16_t opcode = 0;
 
 	const double period = 1.0 / 700;	// The target seconds per instruction
 	double diff = 0,
@@ -82,15 +84,12 @@ int main(int argc, char** argv)
 		clock_gettime(CLOCK_MONOTONIC, &t0);
 
 		// fetch
-		current = *pc++;
-		temp = *pc++;
-		// We shift `temp` so that the bits can be written into the higher
-		// order positions with `&`, giving us a 2-byte instruction.
-		temp <<= 8;
-		current = current | temp;
+		// CHIP-8 instructions are two bytes. Therefore we are reading the
+		// higher order byte first.
+		opcode = (*pc++ & 0xFF) << 8;
+		opcode |= (*pc++ & 0xFF);
 
 		// decode
-
 		// execute
 
 		clock_gettime(CLOCK_MONOTONIC, &t1);
@@ -105,10 +104,9 @@ int main(int argc, char** argv)
 			nanosleep(&wait, NULL);
 		}
 	}
-
 }
 
-void dump_rom(unsigned char* rom, int len)
+void dump_rom(uint8_t* rom, int len)
 {
 	for (int i = 0; i < len; i++) {
 		if (i % 16 == 0) {
@@ -119,7 +117,7 @@ void dump_rom(unsigned char* rom, int len)
 	printf("\n%08x\n", len);
 }
 
-int load_rom(char* path, unsigned char* buffer)
+int load_rom(char* path, uint8_t* buffer)
 {
 	unsigned int len = 0;
 	FILE *fp;
@@ -130,7 +128,7 @@ int load_rom(char* path, unsigned char* buffer)
 	}
 
 	int c = '\0';
-	unsigned char* ram_ptr = buffer;
+	uint8_t* ram_ptr = buffer;
 	for ( ; (c = getc(fp)) != EOF && len < ROM_MAX; ++len) {
 		*(ram_ptr++) = c;
 	}
