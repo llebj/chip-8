e1fcf56c8b91ed83c8523f6bb28aa92416488fac
Joe Bell <35937399+llebj@users.noreply.github.com>
2026-07-31 06:35:37 +0000

    Created struct for chip-8 vm state (wip).

--- chip-8.h
+++ chip-8.h
@@ -13,15 +13,43 @@
 
 #define TARGET_FPS	60
 
+// Use the maximum possible frame buffer size (SuperChip)
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 64
+#define FRAME_BUF_MAX (DISPLAY_WIDTH * DISPLAY_HEIGHT)
+
 void dump_rom(uint8_t* rom, int len);
 int load_rom(char* path, uint8_t* buffer);
+
+// --------
+// Chip-8 -
+// --------
+
+enum Mode {
+	Chip8,
+	SuperChip
+};
+struct chip8 {
+	enum Mode mode;
+	uint8_t memory[MEM_SIZE];
+	uint8_t v[16];
+	uint16_t pc;
+	uint16_t I;
+	uint16_t stack[16];
+	uint8_t stack_pointer;
+	struct framebuffer* frame_buf;
+	uint8_t delay;
+	uint8_t sound;
+};
+struct framebuffer {
+	uint8_t buffer[FRAME_BUF_MAX];
+	uint8_t width;
+	uint8_t height;
+};
 
 // ---------
 // Display -
 // ---------
-
-#define DISPLAY_WIDTH 64
-#define DISPLAY_HEIGHT 32
 
 enum DisplayOp {
 	NOOP,
--- main.c
+++ main.c
@@ -21,12 +21,6 @@
 #include "chip-8.h"
 
 #define NANOSEC 1000000000L
-#define FRAME_BUF_LEN (DISPLAY_HEIGHT * DISPLAY_WIDTH)
-
-enum Mode {
-	Chip8,
-	SuperChip
-};
 
 struct opts {
 	bool show_help;
@@ -34,15 +28,6 @@ struct opts {
 	enum Mode mode;
 	char* file_name;
 };
-
-uint8_t memory[MEM_SIZE];
-uint16_t pc = 0;
-uint16_t I = 0;
-uint16_t stack[16];
-uint8_t stack_pointer = 0;
-uint8_t delay = 0;
-uint8_t sound = 0;
-uint8_t v[16];
 
 uint8_t const font[] = {
 	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -63,13 +48,11 @@ uint8_t const font[] = {
 	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, enum Mode mode);
+void execute_loop(struct chip8* vm, enum DisplayOp* display_op, struct input_state* input_state);
 bool parse_opts(int argc, char** argv, struct opts* opts);
 void show_help(char* file_name);
 
 int rom_size = 0;
-
-uint8_t frame_buf[FRAME_BUF_LEN];
 
 int main(int argc, char** argv)
 {
@@ -77,10 +60,12 @@ int main(int argc, char** argv)
 		show_help(argv[0]);
 		exit(0);
 	}
-	struct opts opts;
-	opts.show_help = false;
-	opts.mode = Chip8;
-	opts.dump_rom = false;
+
+	struct opts opts = {
+		.show_help = false,
+		.mode = Chip8,
+		.dump_rom = false,
+	};
 	if (!parse_opts(argc, argv, &opts)) {
 		printf("Failed to parse program options.\n");
 		show_help(argv[0]);
@@ -92,15 +77,39 @@ int main(int argc, char** argv)
 		exit(0);
 	}
 
+	struct framebuffer framebuffer = {
+		.buffer = {0}
+	};
+	if (opts.mode == Chip8) {
+		framebuffer.height = 32;
+		framebuffer.width = 64;
+	}
+	else {
+		framebuffer.height = 64;
+		framebuffer.width = 128;
+	}
+	struct chip8 vm = {
+		.mode = opts.mode,
+		.memory = {0},
+		.v = {0},
+		.pc = 0,
+		.I = 0,
+		.stack = {0},
+		.stack_pointer = 0,
+		.frame_buf = &framebuffer,
+		.delay = 0,
+		.sound = 0,
+	};
+
 	// Copy the font data into memory starting at `memory + FONT_START`.
-	memcpy(memory + FONT_START, font, sizeof(font));
-	if ((rom_size = load_rom(opts.file_name, memory + ROM_START)) == 0) {
+	memcpy(vm.memory + FONT_START, font, sizeof(font));
+	if ((rom_size = load_rom(opts.file_name, vm.memory + ROM_START)) == 0) {
 		printf("Failed to load ROM.\n");
 		exit(1);
 	}
 
 	if (opts.dump_rom) {
-		dump_rom(memory + ROM_START, rom_size);
+		dump_rom(vm.memory + ROM_START, rom_size);
 		exit(0);
 	}
 
@@ -134,18 +143,18 @@ int main(int argc, char** argv)
 	input_state.last_key = -1;
 
 	// Move the program counter to the start of the ROM
-	pc = ROM_START;
+	vm.pc = ROM_START;
 	while (!quit) {
 		display_op = NOOP;
 		// Most recently pressed key; -1 indicates no key pressed
 		input_state.last_key = -1;
 		t0 = SDL_GetTicksNS();
 
-		if (delay > 0) {
-			--delay;
+		if (vm.delay > 0) {
+			--vm.delay;
 		}
-		if (sound > 0) {
-			--sound;
+		if (vm.sound > 0) {
+			--vm.sound;
 			generate_samples();
 		}
 
@@ -154,11 +163,12 @@ int main(int argc, char** argv)
 		// The emulator loop runs faster than the frame rate; we can fit
 		// `cycles_ps / frames_ps` emulator cycles for each rendered frame.
 		for (int inst = 0; inst < cycles_ps / TARGET_FPS; ++inst) {
-			execute_loop(&display_op, &input_state, opts.mode);
+			execute_loop(&vm, &display_op, &input_state);
 		}
 
 		if (display_op == DRAW) {
-			draw(frame_buf, FRAME_BUF_LEN);
+			draw(vm.frame_buf->buffer,
+				vm.frame_buf->width * vm.frame_buf->height);
 		}
 		else if (display_op == CLEAR) {
 			clear_screen();
@@ -238,8 +248,9 @@ bool parse_opts(int argc, char** argv, struct opts* opts)
  *		      display output operations; this function mutates that
  *		      external state.
  */
-void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, enum Mode mode)
+void execute_loop(struct chip8* vm, enum DisplayOp* display_op, struct input_state* input_state)
 {
+	// TODO: Fix drawing; replace def. labels with frame_buf dims.
 	// TODO: Implement SUPER-CHIP.
 	// TODO: Clean up the flow control in this function.
 	
@@ -249,8 +260,8 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 	// fetch
 	// CHIP-8 instructions are two bytes. Therefore we are reading the
 	// higher order byte first.
-	opcode = (memory[pc++] & 0xFF) << 8;
-	opcode |= (memory[pc++] & 0xFF);
+	opcode = (vm->memory[vm->pc++] & 0xFF) << 8;
+	opcode |= (vm->memory[vm->pc++] & 0xFF);
 
 	// decode
 	// execute
@@ -269,7 +280,7 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 		 * of control, but this section is particularly confusing.
 		 */
 		bool matched = false;
-		if (mode == SuperChip) {
+		if (vm->mode == SuperChip) {
 			switch (opcode & 0x00F0) {
 			case 0x00C0:	// 00CN: Scroll down (SuperChip)
 			{
@@ -282,8 +293,8 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 						// frame buffer.
 						uint16_t x_source = source * DISPLAY_WIDTH + x,
 							 x_target = target * DISPLAY_WIDTH + x;
-						frame_buf[x_target] = frame_buf[x_source];
-						frame_buf[x_source] = 0;
+						vm->frame_buf->buffer[x_target] = vm->frame_buf->buffer[x_source];
+						vm->frame_buf->buffer[x_source] = 0;
 					}
 				}
 				break;
@@ -297,17 +308,17 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 			case 0x00E0:	// 00E0: Clear screen
 				for (uint8_t y = 0; y < DISPLAY_HEIGHT; ++y) {
 					for (uint8_t x = 0; x < DISPLAY_WIDTH; ++x) {
-						frame_buf[y * DISPLAY_WIDTH + x] = 0;
+						vm->frame_buf->buffer[y * DISPLAY_WIDTH + x] = 0;
 					}
 				}
 				*display_op = CLEAR;
 				break;
 			case 0x00EE:	// 00EE: Subroutines
 				// WARN: Potential bounds bug
-				pc = stack[--stack_pointer];
+				vm->pc = vm->stack[--vm->stack_pointer];
 				break;
 			case 0x00FB:	// 00FB: Scroll right (SuperChip)
-				if (mode == SuperChip) {
+				if (vm->mode == SuperChip) {
 					for (uint8_t y = 0; y < DISPLAY_HEIGHT; ++y) {
 						// Scroll is always 4px, so we subtract 5 to get the source
 						// index.
@@ -317,14 +328,14 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 							// frame buffer.
 							uint16_t y_source = y * DISPLAY_WIDTH + source,
 								 y_target = y * DISPLAY_WIDTH + target;
-							frame_buf[y_target] = frame_buf[y_source];
-							frame_buf[y_source] = 0;
+							vm->frame_buf->buffer[y_target] = vm->frame_buf->buffer[y_source];
+							vm->frame_buf->buffer[y_source] = 0;
 						}
 					}
 				}
 				break;
 			case 0x00FC:	// 00FC: Scroll left (SuperChip)
-				if (mode == SuperChip) {
+				if (vm->mode == SuperChip) {
 					for (uint8_t y = 0; y < DISPLAY_HEIGHT; ++y) {
 						for (int32_t source = 4; source < DISPLAY_WIDTH; ++source) {
 							int32_t target = source - 4;
@@ -332,8 +343,8 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 							// frame buffer.
 							uint16_t y_source = y * DISPLAY_WIDTH + source,
 								 y_target = y * DISPLAY_WIDTH + target;
-							frame_buf[y_target] = frame_buf[y_source];
-							frame_buf[y_source] = 0;
+							vm->frame_buf->buffer[y_target] = vm->frame_buf->buffer[y_source];
+							vm->frame_buf->buffer[y_source] = 0;
 						}
 					}
 				}
@@ -352,57 +363,57 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 		break;
 	}
 	case 0x1000:		// 1000: Jump
-		pc = opcode & 0x0FFF;
+		vm->pc = opcode & 0x0FFF;
 		break;
 	case 0x2000:		// 2000: Subroutines
 		// WARN: Potential bounds bug
-		stack[stack_pointer++] = pc;
-		pc = opcode & 0x0FFF;
+		vm->stack[vm->stack_pointer++] = vm->pc;
+		vm->pc = opcode & 0x0FFF;
 		break;
 	case 0x3000:		// 3XNN: Skip conditionally
-		if (v[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) {
-			pc += 2;
+		if (vm->v[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) {
+			vm->pc += 2;
 		}
 		break;
 	case 0x4000:		// 4XNN: Skip conditionally
-		if (v[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
-			pc += 2;
+		if (vm->v[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
+			vm->pc += 2;
 		}
 		break;
 	case 0x5000:		// 5XY0: Skip conditionally
-		if (v[(opcode & 0x0F00) >> 8] == v[(opcode & 0x00F0) >> 4]) {
-			pc += 2;
+		if (vm->v[(opcode & 0x0F00) >> 8] == vm->v[(opcode & 0x00F0) >> 4]) {
+			vm->pc += 2;
 		}
 		break;
 	case 0x6000:		// 6XNN: Set
 		// Set VX to NN
-		v[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+		vm->v[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
 		break;
 	case 0x7000:		// 7XNN: Add
 		// Add NN to VX
-		v[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+		vm->v[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
 		break;
 	case 0x8000:
 		switch (opcode & 0x000F) {
 		case 0x0000:	// 8XY0: Set
-			v[(opcode & 0x0F00) >> 8] = v[(opcode & 0x00F0) >> 4];
+			vm->v[(opcode & 0x0F00) >> 8] = vm->v[(opcode & 0x00F0) >> 4];
 			break;
 		case 0x0001:	// 8XY1: Binary OR
-			v[(opcode & 0x0F00) >> 8] |= v[(opcode & 0x00F0) >> 4];
-			if (mode == Chip8) {
-				v[0xF] = 0;
+			vm->v[(opcode & 0x0F00) >> 8] |= vm->v[(opcode & 0x00F0) >> 4];
+			if (vm->mode == Chip8) {
+				vm->v[0xF] = 0;
 			}
 			break;
 		case 0x0002:	// 8XY2: Binary AND
-			v[(opcode & 0x0F00) >> 8] &= v[(opcode & 0x00F0) >> 4];
-			if (mode == Chip8) {
-				v[0xF] = 0;
+			vm->v[(opcode & 0x0F00) >> 8] &= vm->v[(opcode & 0x00F0) >> 4];
+			if (vm->mode == Chip8) {
+				vm->v[0xF] = 0;
 			}
 			break;
 		case 0x0003:	// 8XY3: Binary XOR
-			v[(opcode & 0x0F00) >> 8] ^= v[(opcode & 0x00F0) >> 4];
-			if (mode == Chip8) {
-				v[0xF] = 0;
+			vm->v[(opcode & 0x0F00) >> 8] ^= vm->v[(opcode & 0x00F0) >> 4];
+			if (vm->mode == Chip8) {
+				vm->v[0xF] = 0;
 			}
 			break;
 		case 0x0004:	// 8XY4: Add
@@ -410,9 +421,9 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 			// Set the carry flag if VX + VY overflows VX
 			uint8_t vx = (opcode & 0x0F00) >> 8,
 				vy = (opcode & 0x00F0) >> 4,
-				carry = (0xFF - v[vx]) < v[vy];
-			v[vx] += v[vy];
-			v[0xF] = carry;
+				carry = (0xFF - vm->v[vx]) < vm->v[vy];
+			vm->v[vx] += vm->v[vy];
+			vm->v[0xF] = carry;
 			break;
 		}
 		case 0x0005:	// 8XY5: Subtract
@@ -420,9 +431,9 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 			// Set the carry flag to 0 if we underflow and 1 otherwise
 			uint8_t vx = (opcode & 0x0F00) >> 8,
 				vy = (opcode & 0x00F0) >> 4,
-				carry = v[vx] >= v[vy];
-			v[vx] -= v[vy];
-			v[0xF] = carry;
+				carry = vm->v[vx] >= vm->v[vy];
+			vm->v[vx] -= vm->v[vy];
+			vm->v[0xF] = carry;
 			break;
 		}
 		case 0x0006:	// 8XY6: Shift
@@ -430,13 +441,13 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 			uint8_t vx = (opcode & 0x0F00) >> 8,
 				vy = (opcode & 0x00F0) >> 4,
 				carry = 0;
-			if (mode == Chip8) {
-				v[vx] = v[vy];
+			if (vm->mode == Chip8) {
+				vm->v[vx] = vm->v[vy];
 			}
 			// Set the flag register to the value of the shifted-out bit.
-			carry = v[vx] & 1;
-			v[vx] >>= 1;
-			v[0xF] = carry;
+			carry = vm->v[vx] & 1;
+			vm->v[vx] >>= 1;
+			vm->v[0xF] = carry;
 			break;
 		}
 		case 0x0007:	// 8XY7: Subtract
@@ -444,9 +455,9 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 			// Set the carry flag to 0 if we underflow and 1 otherwise
 			uint8_t vx = (opcode & 0x0F00) >> 8,
 				vy = (opcode & 0x00F0) >> 4,
-				carry = v[vy] >= v[vx];
-			v[vx] = v[vy] - v[vx];
-			v[0xF] = carry;
+				carry = vm->v[vy] >= vm->v[vx];
+			vm->v[vx] = vm->v[vy] - vm->v[vx];
+			vm->v[0xF] = carry;
 			break;
 		}
 		case 0x000E:	// 8XYE: Shift
@@ -455,56 +466,56 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 				vy = (opcode & 0x00F0) >> 4,
 				carry = 0;
 			// Set the flag register to the value of the shifted-out bit.
-			if (mode == Chip8) {
-				carry = (v[vy] & 0x80) >> 7;
-				v[vx] = v[vy] << 1;
+			if (vm->mode == Chip8) {
+				carry = (vm->v[vy] & 0x80) >> 7;
+				vm->v[vx] = vm->v[vy] << 1;
 			}
 			else {
-				carry = (v[vx] & 0x80) >> 7;
-				v[vx] <<= 1;
+				carry = (vm->v[vx] & 0x80) >> 7;
+				vm->v[vx] <<= 1;
 			}
-			v[0xF] = carry;
+			vm->v[0xF] = carry;
 			break;
 		}
 		}
 		break;
 	case 0x9000:		// 9XY0: Skip conditionally
-		if (v[(opcode & 0x0F00) >> 8] != v[(opcode & 0x00F0) >> 4]) {
-			pc += 2;
+		if (vm->v[(opcode & 0x0F00) >> 8] != vm->v[(opcode & 0x00F0) >> 4]) {
+			vm->pc += 2;
 		}
 		break;
 	case 0xA000:		// ANNN: Set index
 		// Set index to NNN
-		I = opcode & 0x0FFF; 
+		vm->I = opcode & 0x0FFF; 
 		break;
 	case 0xB000:		// BNNN: Jump with offset
-		pc = (opcode & 0x0FFF) + v[mode == Chip8 ? 0 : (opcode & 0x0F00) >> 8];
+		vm->pc = (opcode & 0x0FFF) + vm->v[vm->mode == Chip8 ? 0 : (opcode & 0x0F00) >> 8];
 		break;
 	case 0xC000:		// CXNN: Random
-		v[(opcode & 0x0F00) >> 8] = SDL_rand(UINT16_MAX) & (opcode & 0x00FF);
+		vm->v[(opcode & 0x0F00) >> 8] = SDL_rand(UINT16_MAX) & (opcode & 0x00FF);
 		break;
 	case 0xD000:
 	{
 		switch (opcode & 0x000F) {
 		case 0x0000:	// DXY0: Draw 16x16 (SuperChip)
-			if (mode == SuperChip) {
+			if (vm->mode == SuperChip) {
 				// TODO: Implement.
 				break;
 			}
 		default:	// DXYN: Draw
 		{
 			// Variables used to store values from the display instruction.
-			uint8_t sx = v[(opcode & 0x0F00) >> 8] % DISPLAY_WIDTH,
-				sy = v[(opcode & 0x00F0) >> 4] % DISPLAY_HEIGHT,
+			uint8_t sx = vm->v[(opcode & 0x0F00) >> 8] % DISPLAY_WIDTH,
+				sy = vm->v[(opcode & 0x00F0) >> 4] % DISPLAY_HEIGHT,
 				height = opcode & 0x000F,
 				line = 0;
-			v[0xF] = 0;
+			vm->v[0xF] = 0;
 
 			// We want to write the sprite data to N lines, starting at
 			// Y. We need to ensure that we clip any lines that exceed
 			// the height of the raster.
 			for (uint8_t y = 0; y < height && sy + y < DISPLAY_HEIGHT; ++y) {
-				line = memory[I + y];
+				line = vm->memory[vm->I + y];
 				// We want to write the sprite data at `I + y` onto the
 				// current line, starting at X. We need to clip
 				// the data if it exceeds the width of the raster.
@@ -516,10 +527,10 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 					if ((line & (0x80 >> x)) == 0) {
 						continue;
 					}
-					if (frame_buf[(sy + y) * DISPLAY_WIDTH + (sx + x)] == 1) {
-						v[0xF] = 1;
+					if (vm->frame_buf->buffer[(sy + y) * DISPLAY_WIDTH + (sx + x)] == 1) {
+						vm->v[0xF] = 1;
 					}
-					frame_buf[(sy + y) * DISPLAY_WIDTH + (sx + x)] ^= 1;
+					vm->frame_buf->buffer[(sy + y) * DISPLAY_WIDTH + (sx + x)] ^= 1;
 				}
 			}
 			*display_op = DRAW;
@@ -530,13 +541,13 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 	case 0xE000:
 		switch (opcode & 0x0FF) {
 		case 0x009E:	// E09E: Skip if key
-			if (input_state->kb_state[v[(opcode & 0x0F00) >> 8]]) {
-				pc += 2;
+			if (input_state->kb_state[vm->v[(opcode & 0x0F00) >> 8]]) {
+				vm->pc += 2;
 			}
 			break;
 		case 0x00A1:	// E0A1: Skip if key
-			if (!input_state->kb_state[v[(opcode & 0x0F00) >> 8]]) {
-				pc += 2;
+			if (!input_state->kb_state[vm->v[(opcode & 0x0F00) >> 8]]) {
+				vm->pc += 2;
 			}
 			break;
 		default:
@@ -545,31 +556,31 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 	case 0xF000:
 		switch (opcode & 0x00FF) {
 		case 0x0007:	// FX07: Store delay
-			v[(opcode & 0x0F00) >> 8] = delay;
+			vm->v[(opcode & 0x0F00) >> 8] = vm->delay;
 			break;
 		case 0x000A:	// FX0A: Get key
 			if (input_state->last_key == -1) {
 				// Halt execution
-				pc -= 2;
+				vm->pc -= 2;
 			}
 			else {
-				v[(opcode & 0x0F00) >> 8] = input_state->last_key;
+				vm->v[(opcode & 0x0F00) >> 8] = input_state->last_key;
 			}
 			break;
 		case 0x0015:	// FX15: Read delay
-			delay = v[(opcode & 0x0F00) >> 8];
+			vm->delay = vm->v[(opcode & 0x0F00) >> 8];
 			break;
 		case 0x0018:	// FX18: Store sound
-			sound = v[(opcode & 0x0F00) >> 8];
+			vm->sound = vm->v[(opcode & 0x0F00) >> 8];
 			break;
 		case 0x001E:	// FX1E: Add to index
-			I += v[(opcode & 0x0F00) >> 8];
+			vm->I += vm->v[(opcode & 0x0F00) >> 8];
 			break;
 		case 0x0029:	// FX29: Font character
 			// The bit pattern `0x0300` masks off the first nibble
 			// of X.
 			// Font characters are 5 bytes long.
-			I = FONT_START + 5 * ((opcode & 0x0300) >> 8);
+			vm->I = FONT_START + 5 * ((opcode & 0x0300) >> 8);
 			break;
 		case 0x0030:	// FX30: Set large hex character (SuperChip)
 			// TODO: Implement.
@@ -578,8 +589,8 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 		{
 			// uint8_t max is 255 so we are always going to be storing
 			// 3 digits.
-			for (uint8_t i = 3, vx = v[(opcode & 0x0F00) >> 8]; i > 0; --i, vx /= 10) {
-				memory[I + i - 1] = vx % 10;
+			for (uint8_t i = 3, vx = vm->v[(opcode & 0x0F00) >> 8]; i > 0; --i, vx /= 10) {
+				vm->memory[vm->I + i - 1] = vx % 10;
 			}
 			break;
 		}
@@ -587,10 +598,10 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 		{
 			uint8_t registers = ((opcode & 0x0F00) >> 8) + 1;
 			for (uint8_t i = 0; i < registers; ++i) {
-				memory[I + i] = v[i];
+				vm->memory[vm->I + i] = vm->v[i];
 			}
-			if (mode == Chip8) {
-				I += registers;
+			if (vm->mode == Chip8) {
+				vm->I += registers;
 			}
 			break;
 		}
@@ -598,10 +609,10 @@ void execute_loop(enum DisplayOp* display_op, struct input_state* input_state, e
 		{
 			uint8_t registers = ((opcode & 0x0F00) >> 8) + 1;
 			for (uint8_t i = 0; i < registers; ++i) {
-				v[i] = memory[I + i];
+				vm->v[i] = vm->memory[vm->I + i];
 			}
-			if (mode == Chip8) {
-				I += registers;
+			if (vm->mode == Chip8) {
+				vm->I += registers;
 			}
 			break;
 		}
